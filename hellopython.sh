c6d572e93a7fa2cd42abc389b93e5f6e64e2d9f5
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-03-12 19:48:46 -0300

    uytioyg

---
+++ hellopython.sh
@@ -0,0 +1,86 @@
+export GITHUBUSERNAME="maridasi47500"
+$(git clone git@github.com:$GITHUBUSERNAME/$1.git)
+echo "__pycache__/" >> "$1/.gitignore" 
+mkdir "$1/templates" 
+echo "`cat <<EOF
+<h1># $1</h1>
+{% for x in users | reverse %}
+{% endfor %}" 
+EOF`" >> "$1/templates/hey.html" 
+echo "`cat <<EOF
+import sqlite3
+from flask import g
+
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+EOF`" >> "$1/yourappdb.py"
+echo "`cat <<EOF
+from flask import Flask, render_template
+from yourappdb import query_db, get_db, close_connection
+
+app = Flask(__name__)
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+init_db()
+
+@app.route("/")
+def hello_world():
+    user = query_db('select * from contacts'):
+    one_user = query_db('select * from contacts where username = ?',
+                [the_username], one=True)
+    return render_template("hey.html", users=user, one_user=one_user)
+EOF`" >> "$1/app.py"
+echo "`cat <<EOF
+CREATE TABLE contacts (
+	contact_id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	phone TEXT NOT NULL UNIQUE
+);
+CREATE TABLE groups (
+   group_id INTEGER PRIMARY KEY,
+   name TEXT NOT NULL
+);
+CREATE TABLE contact_groups(
+   contact_id INTEGER,
+   group_id INTEGER,
+   PRIMARY KEY (contact_id, group_id),
+   FOREIGN KEY (contact_id) 
+      REFERENCES contacts (contact_id) 
+         ON DELETE CASCADE 
+         ON UPDATE NO ACTION,
+   FOREIGN KEY (group_id) 
+      REFERENCES groups (group_id) 
+         ON DELETE CASCADE 
+         ON UPDATE NO ACTION
+);
+EOF`" >> "$1/schema.sql"
+$(cd $1/)

232604adc9cdae221b6de48757ebd2c9c468ed54
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-03-13 23:46:29 -0300

    dfg

--- scaffold.py
+++ scaffold.py
@@ -69,13 +69,13 @@ with open("app.py", "a") as myfile:
 with open("schema.sql", "a") as myfile:
     myfile.write(mystr.format(filename=filename))
 with open("templates/hey.html", "a") as myfile:
-    myfile.write("{% block liens %}"+"<a href=\"/add_one_{filename}\"> add one {filename}</a>".format(filename=filename)+"{% endblock %}")
+    myfile.write("{% block liens %}<a href=\"/\">bienvenue</a>"+"<a href=\"/add_one_{filename}\"> add one {filename}</a>".format(filename=filename)+"{% endblock %}")
 
 
 with open("templates/"+filename+"form.html", "w") as myfile:
     myfile.write(formhtml.format(filename=filename)+"<div class=\"actions\"><input type=\"submit\"/></div></form>" + "{% for x in "+filename+"s %}{{"+ "x[\""+items[2]+"\"] }}{% endfor %}")
 with open("templates/"+filename+"form.html", "a") as myfile:
-    myfile.write("{% block liens %}"+"<a href=\"/add_one_{filename}\"> add one {filename}</a>".format(filename=filename)+"{% endblock %}")
+    myfile.write("{% block liens %}<a href=\"/\">bienvenue</a>"+"<a href=\"/add_one_{filename}\"> add one {filename}</a>".format(filename=filename)+"{% endblock %}")
 
 
 
